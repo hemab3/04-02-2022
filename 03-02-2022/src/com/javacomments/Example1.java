fb2615cbdd3e50dd420d81e2f358ee38c524ec05
HEMA <HEMA@DESKTOP-GCL3K6A>
2022-02-04 10:36:47 +0530

    first commit

---
+++ 03-02-2022/src/com/javacomments/Example1.java
@@ -0,0 +1,18 @@
+//user defined package name=>it is single line comment
+package com.javacomments;
+
+/*
+ * user defined class name,
+ * 
+ * we can declare user defined class name with class
+ * 
+ * Example1 is user defined class name.
+ */
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+	   System.out.println("Hello Java");	
+	}
+
+}
